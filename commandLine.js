8b6aac92773fbb2f634a5ed3f2365e870207bd1b
jakerawlings37 <jakerawlings37@gmail.com>
2023-12-05 18:16:23 -0500

    working and almost done

--- commandLine.js
+++ commandLine.js
@@ -2,7 +2,6 @@ let commandLine;
 let stage = 'start';
 
 const networkScan = `
-
 PORT     STATE SERVICE  VERSION
 80/tcp   open  http     Apache httpd 2.4.6 ((CentOS) OpenSSL/1.0.2k-fips PHP/8.0.30)
 |_http-server-header: Apache/2.4.6 (CentOS) OpenSSL/1.0.2k-fips PHP/8.0.30
@@ -19,6 +18,48 @@ PORT     STATE SERVICE  VERSION
 3306/tcp open  mysql    MySQL (unauthorized)
 
 `
+
+const end = `
+
+8888888 8888888888 8 8888        8 8 8888888888             8 8888888888   b.             8 8 888888888o.      
+      8 8888       8 8888        8 8 8888                   8 8888         888o.          8 8 8888    \`^888.   
+      8 8888       8 8888        8 8 8888                   8 8888         Y88888o.       8 8 8888        \`88. 
+      8 8888       8 8888        8 8 8888                   8 8888         .\`Y888888o.    8 8 8888         \`88 
+      8 8888       8 8888        8 8 888888888888           8 888888888888 8o. \`Y888888o. 8 8 8888          88 
+      8 8888       8 8888        8 8 8888                   8 8888         8\`Y8o. \`Y88888o8 8 8888          88 
+      8 8888       8 8888888888888 8 8888                   8 8888         8   \`Y8o. \`Y8888 8 8888         ,88 
+      8 8888       8 8888        8 8 8888                   8 8888         8      \`Y8o. \`Y8 8 8888        ,88' 
+      8 8888       8 8888        8 8 8888                   8 8888         8         \`Y8o.\` 8 8888    ,o88P'   
+      8 8888       8 8888        8 8 888888888888           8 888888888888 8            \`Yo 8 888888888P'  
+
+`
+
+const SHELLCODE = `
+\\x29\\xc9\\x83\\xe9\\xef\\xe8\\xff\\xff\\xff\\xff\\xc0\\x5e\\x81\\x76\\x0e\\x87\\x95\\x82\\x07\\x83\\xee\\xfc\\xe2
+\\xf4\\xb6\\x4e\\x75\\xe4\\xd4\\xd6\\xd1\\x6d\\x85\\x1c\\x63\\xb7\\xe1\\x58\\x02\\x94\\xde\\x25\\xbd\\xca\\x07\\xdc
+\\xfb\\xfe\\xef\\x55\\x2a\\x06\\xb0\\xfd\\x80\\x07\\x96\\xce\\x0b\\xe6\\x37\\xf3\\xd2\\x56\\xd4\\x26\\x81\\x8e\\x66
+\\x58\\x02\\x55\\xef\\xfb\\xad\\x74\\xef\\xfd\\xad\\x28\\xe5\\xfc\\x0b\\xe4\\xd5\\xc6\\x0b\\xe6\\x37\\x9e\\x4f\\x87
+
+`;
+
+const NOP_SLED = `
+
+\\x90\\x90\\x90\\x90\\x90\\x90\\x90\\x90\\x90\\x90\\x90\\x90\\x90\\x90\\x90\\x90\\x90\\x90\\x90\\x90\\x90\\x90\\x90
+\\x90\\x90\\x90\\x90\\x90\\x90\\x90\\x90\\x90\\x90\\x90\\x90\\x90\\x90\\x90\\x90\\x90\\x90\\x90\\x90\\x90\\x90\\x90
+
+`;
+
+const RETURN_ADDRESS = `
+
+\\x96\\xc4\\xff\\xff
+
+`;
+
+const bytes = {
+    SHELLCODE: SHELLCODE,
+    NOP_SLED: NOP_SLED,
+    RETURN_ADDRESS: RETURN_ADDRESS
+};
 
 function createCommandLine(showPrompt = true) {
     const consoleDiv = document.getElementById('console');
@@ -112,6 +153,7 @@ async function processCommand(stage, command) {
                 document.body.innerHTML = '<div id="console"><div id="output"></div></div>';
                 createCommandLine(false);
                 await consolePrint('MISSION ACCEPTED', false);
+                await consolePrint('THE FIRST THING WE NEED TO DO IS SCAN THE NETWORK FOR A WAY IN', false);
                 await consolePrint('PRESS ENTER TO START SCANNING THE NETWORK');
             }
             break;
@@ -123,13 +165,13 @@ async function processCommand(stage, command) {
             await consolePrint('LOOKS LIKE THERE IS A WEB SERVER RUNNING ON PORT 443', false);
             await consolePrint('WORD AROUND THE NSA IS THAT AN INTERN FROM BU WROTE THE WEB APPLICATION CODE', false);
             await consolePrint('ITS PROBABLY VULNERABLE TO SQL INJECTION', false);
-            await consolePrint('WE NEED TO SEND A POST REQUEST WITH SQL CODE THAT ALWAYS RETURNS TRUE', false);
+            await consolePrint('WE NEED TO SEND A POST REQUEST TO BYPASS AUTHENTICATION', false);
             await consolePrint('ENTER A PASSWORD THAT, WHEN TREATED AS SQL CODE, WILL ALWAYS RETURN TRUE ');
             setStage('sqli');
             break;
         case 'sqli':
-            await consolePrint('SENDING POST REQUEST WITH PAYLOAD', false);
-            await consolePrint('SERVER SIDE AUTHENTICATION CODE: SELECT USERS FROM USER WHERE USERNAME = \'ADMIN\' AND PASSWORD =\' ' + command + '\'', false);
+            await consolePrint('SENDING POST REQUEST WITH PAYLOAD: ' + command, false, 0);
+            await consolePrint('SERVER SIDE AUTHENTICATION CODE: SELECT USERS FROM USER WHERE USERNAME = \'BU_INTERN\' AND PASSWORD =\' ' + command + '\'', false, 0);
             const strippedCommand = command.replace(/\s+/g, "").toLowerCase();
             console.log(strippedCommand);
             if (strippedCommand == "'or1=1--"
@@ -140,13 +182,77 @@ async function processCommand(stage, command) {
             ) {
                 document.body.innerHTML = '<div id="console"><div id="output"></div></div>';
                 createCommandLine(false);
-                await consolePrint('AUTHENTICATION BYPASSED!', false);
-                await consolePrint('LOGGED IN AS ADMIN', );
+                await consolePrint('AUTHENTICATION BYPASSED', false);
+                await consolePrint('LOGGED IN AS BU_INTERN', false);
+                await consolePrint('NOW THAT WE ARE IN THE SYSTEM, WE NEED TO ESCALATE OUR PRIVILEGES', false);
+                await consolePrint('IT LOOKS LIKE THE BU INTERN WAS WRITING A PROGRAM TO TRICK THE AI INTO THINKING BU IS BETTER THAN NORTHEASTERN', false);
+                await consolePrint('MISINFORMATION.EXE', false);
+                await consolePrint('THE PROGRAM RUNS AS ROOT, SO WE CAN STOP THE AI IF WE CAN SOMEHOW INJECT CODE INTO PROGRAM\'S MEMORY', false);
+                await consolePrint('THE FOLLOWING SHELLCODE SHOULD DESTROY THE AI FOR GOOD', false);
+                printAsciiArt(SHELLCODE, true, 1);
+                await consolePrint('WE NEED TO CREATE USER INPUT TO OVERFLOW THE BUFFER AND EXECUTE THE SHELLCODE', false);
+                await consolePrint('WE HAVE THE FOLLOWING COMPONENTS:', false);
+                await consolePrint('SHELLCODE', false);
+                await consolePrint('NOP_SLED', false);
+                await consolePrint('RETURN_ADDRESS', false);
+                await consolePrint('IN WHAT ORDER SHOULD WE ARRANGE THESE COMPONENTS TO INJECT OUR SHELLCODE? (SEPERATED BY SPACES)');
                 setStage('challenge2');
             } else {
-                await consolePrint('AUTHENTICATION CHECK FAILED. TRY AGAIN');
+                await consolePrint('AUTHENTICATION CHECK FAILED. TRY AGAIN', true, 0);
             }
             break;
+        case 'challenge2':
+            const order = command.toUpperCase().split(" ");
+            if (order.length !== 3) {
+                await consolePrint('INVALID INPUT. TRY AGAIN');
+                break;
+            } else {
+                let valid = false;
+                for (let i = 0; i < order.length; i++) {
+                    if (order[i] !== 'SHELLCODE' && order[i] !== 'NOP_SLED' && order[i] !== 'RETURN_ADDRESS') {
+                        await consolePrint('INVALID INPUT. TRY AGAIN');
+                        break;
+                    } else {
+                        valid = true;
+                    }
+                }
+                if (!valid) {
+                    break;
+                }
+            }
+            document.body.innerHTML = '<div id="console"><div id="output"></div></div>';
+            createCommandLine(false);
+            await consolePrint('CREATING PAYLOAD IN FOLLOWING ORDER: ' + order[0] + ' ' + order[1] + ' ' + order[2], false, 0);
+            await consolePrint('INJECTING CODE...', false, 0);
+            await sleep(2000);
+            await consolePrint('PROGRAM MEMORY DUMP', false, 0);
+            const payload = bytes[order[0]].replace(/\s+/g, "") + bytes[order[1]].replace(/\s+/g, "") + bytes[order[2]].replace(/\s+/g, "");
+
+            let mem_addr = 3432654734;
+            let mem_dump = '';
+            for (let i = 0; i < payload.length / 60 - 1; i++) {
+                mem_dump += mem_addr.toString();
+                mem_dump += '\t';
+                for (let j = 0; j < 80; j++) {
+                    mem_dump += payload[i * 60 + j];
+                }
+                mem_addr += 437289;
+                mem_dump += '\n';
+            }
+            printAsciiArt(mem_dump, true, 1.2);
+
+            if (order[0] === 'NOP_SLED' && order[1] === 'SHELLCODE' && order[2] === 'RETURN_ADDRESS') {
+                await consolePrint('INJECTION SUCCESSFUL', false);
+                await consolePrint('SHELLCODE EXECUTED', false);
+                await consolePrint('CONGRATULATIONS! YOU HAVE KILLED ROKO AND SAVED THE WORLD', false);
+                await consolePrint('NOW THE NSA CAN GO BACK TO SPYING ON US', false);
+                await consolePrint('AND REMEMEBER, NEVER TRUST AN INTERN FROM BU', false);
+                printAsciiArt(end);
+            } else {
+                await consolePrint('INJECTION FAILED. PLEASE TRY AGAIN', true);
+                break;
+            }
+
     }
 }
 
@@ -154,15 +260,20 @@ async function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-async function consolePrint(message, printConsole = true, ms = 1) {
+async function consolePrint(message, printConsole = true, ms = 35) {
     console.log('Got here in consolePrint: ', message);
     const messageDisplay = document.createElement('div');
     // messageDisplay.textContent = message;
 
-    for (let i = 0; i < message.length; i++) {
-        messageDisplay.textContent += message[i];
+    if (ms === 0) {
+        messageDisplay.textContent = message;
         commandLine.appendChild(messageDisplay);
-        await sleep(ms);
+    } else {
+        for (let i = 0; i < message.length; i++) {
+            messageDisplay.textContent += message[i];
+            commandLine.appendChild(messageDisplay);
+            await sleep(ms);
+        }
     }
 
     // commandLine.appendChild(messageDisplay);
